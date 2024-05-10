aa9f8c11aa4842605c825dcdaeb17be4c017f205
Eduard <langedu1@students.zhaw.ch>
2024-05-10 15:58:00 +0200

    Changes in Name and Button

--- frontend/src/app/gui-model/guimodel.ts
+++ frontend/src/app/gui-model/guimodel.ts
@@ -3,7 +3,7 @@ export class GuiModel {
 
     private _guiModel = {
         "application": {
-            "title": "DevOpsDemo FS2024",
+            "title": "DevOps Eduard",
             "formList": [
                 {
                     "id": "OwnUserForm",
@@ -125,6 +125,14 @@ export class GuiModel {
                                 "form" : "ToDoForm"
                             }
                         },
+                        {
+                            "type": "button",
+                            "name": { default: "TestKachel" },
+                            "icon": "fa-group",
+                            "color": "red",
+                            "page": "modulePage",
+                            "width": 2,
+                           },
                         {
                             "type": "list",
                             "name": "ToDo",
