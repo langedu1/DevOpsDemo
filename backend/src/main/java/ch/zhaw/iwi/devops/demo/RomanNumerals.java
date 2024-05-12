2d5dda2da68e5d80660098d54844deb71fee0cfb
Eduard <langedu1@students.zhaw.ch>
2024-05-12 10:14:47 +0200

    Commit Number 5

--- backend/src/main/java/ch/zhaw/iwi/devops/demo/RomanNumerals.java
+++ backend/src/main/java/ch/zhaw/iwi/devops/demo/RomanNumerals.java
@@ -2,13 +2,16 @@ package ch.zhaw.iwi.devops.demo;
 
 public class RomanNumerals {
     public String toRoman(int number) {
-        if (number == 3) {
+        if (number == 4) {
+            return "IV";
+        } else if (number == 3) {
             return "III";
         } else if (number == 2) {
             return "II";
         }
         return "I";
     }
+    
 }
     
 
