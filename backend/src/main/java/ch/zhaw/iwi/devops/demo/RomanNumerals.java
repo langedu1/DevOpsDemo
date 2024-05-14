88603c26c05e4675d25785d43ef8a5baad60cd58
Eduard <langedu1@students.zhaw.ch>
2024-05-14 14:38:36 +0200

    Commit Code Smell Change

--- backend/src/main/java/ch/zhaw/iwi/devops/demo/RomanNumerals.java
+++ backend/src/main/java/ch/zhaw/iwi/devops/demo/RomanNumerals.java
@@ -1,26 +1,7 @@
 package ch.zhaw.iwi.devops.demo;
 
 public class RomanNumerals {
-  /*  public String toRoman(int number) {
-        if (number >= 50) {
-            return "L" + toRoman(number - 50);
-        } else if (number >= 10) {
-            return "X" + toRoman(number - 10);
-        } else if (number == 9) {
-            return "IX";
-        } else if (number == 6) {
-            return "VI";
-        } else if (number == 5) {
-            return "V";
-        } else if (number == 4) {
-            return "IV";
-        } else if (number == 3) {
-            return "III";
-        } else if (number == 2) {
-            return "II";
-        }
-        return "I";
-    }*/ 
+ 
 
     public String toRoman(int number) {
         String roman = "";
